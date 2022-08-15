6729baa61af5eda0c8ecc9236119a58435be7de0
Horndt <mattias.horndt@mercedes-benz.com>
2022-08-15 13:08:04 +0200

    create Books.js and fill content

---
+++ src/components/Books.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Book = ({ BookChangeShelf, book }) => {
+  let image = book.imageLink
+    ? book.imageLink.thumb
+    : "https://www.deskmodder.de/phpBB3/files/vorschau/38_No_Cover_Jyrik.png";
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${image})`,
+            }}
+          ></div>
+          <div className="book-shelf-changer">
+            <select>
+              <option disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">
+          {book.authors ? book.authors.join(", ") : "Author unknown :( "}
+        </div>
+      </div>
+    </li>
+  );
+};
+
+Book.PropTypes = {
+  BookChangeShelf: PropTypes.func.isRequired,
+  book: PropTypes.object.isRequired,
+};
+
+export default Book;
